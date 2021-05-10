d441389bca1252025dfc4302b75dd68f2c8adf0b
Arthur <arthurpontiervalois@gmail.com>
2021-05-10 02:37:29 -0400

    Update

--- src/front/ast/parser.ml
+++ src/front/ast/parser.ml
@@ -4,15 +4,17 @@ open LilyFront.Error
 
 module Token = LilyFront.Token
 
-let next_token ast =
-    ast.pos <- ast.pos + 1;
-    ast.current_token <- CCVector.get ast.stream.tok ast.pos;
-    ast.current_location <- CCVector.get ast.stream.loc ast.pos;
-    ()
+module UtilParser = struct
+    let next_token ast =
+        ast.pos <- ast.pos + 1;
+        ast.current_token <- CCVector.get ast.stream.tok ast.pos;
+        ast.current_location <- CCVector.get ast.stream.loc ast.pos;
+        ()
 
-let get_next_token ast = CCVector.get ast.stream.tok (ast.pos+1)
+    let get_next_token ast = CCVector.get ast.stream.tok (ast.pos+1)
 
-let get_previous_token ast = CCVector.get ast.stream.tok (ast.pos-1)
+    let get_previous_token ast = CCVector.get ast.stream.tok (ast.pos-1)
+end
 
 module ParseExpr = struct
     let parse_expr_value tok =
@@ -27,8 +29,8 @@ module ParseExpr = struct
 
     (* + - * / % ^ *)
     let parse_binop_operator ast =
-      let left = parse_expr_value (get_previous_token ast) in
-      let right = parse_expr_value (get_next_token ast) in
+      let left = parse_expr_value (UtilParser.get_previous_token ast) in
+      let right = parse_expr_value (UtilParser.get_next_token ast) in
       match (left, right) with
       | (Ok x1, Ok x2) -> (match token_to_binop ast.current_token with
                            | Ok b -> Ok (Expr (ExprBinop (x1,b,x2)))
@@ -55,8 +57,8 @@ let run_parser ast =
       match parser ast with
       | Some (Error _) -> Printf.printf "error\n"
       | Some (Ok p) -> (Printf.printf "%s\n" (ast_kind_to_str (p));
-                        next_token ast;
+                        UtilParser.next_token ast;
                         loop (ast))
-      | None -> (next_token ast;
+      | None -> (UtilParser.next_token ast;
                  loop (ast)) in
   loop (ast)
--- src/front/ast/parser.mli
+++ src/front/ast/parser.mli
@@ -2,15 +2,16 @@ open Ast
 open LilyFront.Token
 open LilyFront.Error
 
-val next_token : 'a ast -> unit
-
-val get_next_token : 'a ast -> 'a token
-val get_previous_token : 'a ast -> 'a token
+module UtilParser : sig
+    val next_token : 'a ast -> unit
+    val get_next_token : 'a ast -> 'a token
+    val get_previous_token : 'a ast -> 'a token
+end
 
 module ParseExpr : sig
-  val parse_expr_value : 'a token -> ('a expr, error_id) result
-  val parse_binop_operator : 'a ast -> ('a ast_kind, error_id) result
-  (*val parse_expr : 'a ast -> 'a ast_kind*)
+    val parse_expr_value : 'a token -> ('a expr, error_id) result
+    val parse_binop_operator : 'a ast -> ('a ast_kind, error_id) result
+    (*val parse_expr : 'a ast -> 'a ast_kind*)
 end
 
 module ParseStmt : sig
