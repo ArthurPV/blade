6402012677f694a920c1fe7db051f1ac557cdbbe
Arthur <arthurpontiervalois@gmail.com>
2021-05-12 13:46:33 -0400

    Update

--- tests/lexer/lexer_test.ml
+++ tests/lexer/lexer_test.ml
@@ -1,10 +1,12 @@
 open OUnit2
 
-
 let operator_suite =
-  "operator_suite">:::
-      ["test_plus">:: Operator.test_plus;
-       "test_minus">:: Operator.test_minus]
+    "operator_suite">:::
+        ["test_plus">:: Operator.test_plus;
+         "test_minus">:: Operator.test_minus;
+         "test_star">:: Operator.test_star;
+         "test_slash">:: Operator.test_slash;
+         "test_percentage">:: Operator.test_percentage]
 
 let _ =
-  run_test_tt_main operator_suite
+    run_test_tt_main operator_suite
--- tests/lexer/operator.ml
+++ tests/lexer/operator.ml
@@ -4,16 +4,37 @@ open LilyFront.Stream
 
 (* + ++ += *)
 let test_plus test_ctxt =
-  let st = new_stream_token in
-  Utils.test "./inputs/plus.li";
-  assert_equal (CCVector.get st.tok 0) (Operator OperatorPlusEq);
-  assert_equal (CCVector.get st.tok 1) (Operator OperatorPlusPlus);
-  assert_equal (CCVector.get st.tok 2) (Operator OperatorPlus)
+    let st = new_stream_token in
+    Utils.test "inputs/operators/plus.li";
+    assert_equal (CCVector.get st.tok 0) (Operator OperatorPlusEq);
+    assert_equal (CCVector.get st.tok 1) (Operator OperatorPlusPlus);
+    assert_equal (CCVector.get st.tok 2) (Operator OperatorPlus)
 
 (* - -- -= *)
 let test_minus test_ctxt =
-  let st = new_stream_token in
-  Utils.test "./inputs/minus.li";
-  assert_equal (CCVector.get st.tok 0) (Operator OperatorMinusEq);
-  assert_equal (CCVector.get st.tok 1) (Operator OperatorMinusMinus);
-  assert_equal (CCVector.get st.tok 2) (Operator OperatorMinus)
+    let st = new_stream_token in
+    Utils.test "inputs/operators/minus.li";
+    assert_equal (CCVector.get st.tok 0) (Operator OperatorMinusEq);
+    assert_equal (CCVector.get st.tok 1) (Operator OperatorMinusMinus);
+    assert_equal (CCVector.get st.tok 2) (Operator OperatorMinus)
+
+(* *= * *)
+let test_star test_ctxt = 
+    let st = new_stream_token in
+    Utils.test "inputs/operators/star.li";
+    assert_equal (CCVector.get st.tok 0) (Operator OperatorStarEq);
+    assert_equal (CCVector.get st.tok 1) (Operator OperatorStar)
+
+(* /= / *)
+let test_slash test_ctxt = 
+    let st = new_stream_token in
+    Utils.test "inputs/operators/slash.li";
+    assert_equal (CCVector.get st.tok 0) (Operator OperatorSlashEq);
+    assert_equal (CCVector.get st.tok 1) (Operator OperatorSlash)
+
+(* %= % *)
+let test_percentage test_ctxt = 
+    let st = new_stream_token in
+    Utils.test "inputs/operators/percentage.li";
+    assert_equal (CCVector.get st.tok 0) (Operator OperatorPercentageEq);
+    assert_equal (CCVector.get st.tok 1) (Operator OperatorPercentage)
