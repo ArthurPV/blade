bc29185ac47a2a0d08a018595c4f0a6c6a14efe8
Arthur <arthurpontiervalois@gmail.com>
2021-05-25 09:28:15 -0400

    Add type error

--- src/front/error.ml
+++ src/front/error.ml
@@ -20,6 +20,26 @@ type error_id =
     | ErrorIdUnexpectedAst
     | ErrorIdSyntaxError
 
+type error = {
+    mutable id: error_id CCVector.vector;
+    mutable line: int CCVector.vector;
+    mutable col: int CCVector.vector;
+    mutable count: int;
+}
+
+let new_error = {
+    id = CCVector.create ();
+    line = CCVector.create ();
+    col = CCVector.create ();
+    count = 0;
+}
+
+let push_error error id line col = 
+    CCVector.push error.id id;
+    CCVector.push error.line line;
+    CCVector.push error.col col;
+    error.count <- error.count + 1
+
 let error_id_to_str id =
     match id with
     | ErrorIdUnexpectedToken s -> Printf.sprintf "unexpected token: \'%s\'" s
@@ -47,3 +67,11 @@ let print_error err line col filename =
     Printf.printf "\027[1mFile \"%s\", location %d:%d\027[0m\n" filename line col;
     Printf.printf "\027[1m\027[31mError\027\027[0m\027[1m: %s\n" (error_id_to_str err);
     exit 1
+
+let print_errors error filename i = 
+    let rec loop i = 
+        if i < (CCVector.length error.id)-1 then
+            Printf.printf "\027[1mFile \"%s\", location %d:%d\027[0m\n" filename (CCVector.get error.line i) (CCVector.get error.col i);
+            Printf.printf "\027[1m\027[31mError\027\027[0m\027[1m: %s\n" (error_id_to_str (CCVector.get error.id i));
+            loop (i+1) in
+    loop (i+1);
--- src/front/error.mli
+++ src/front/error.mli
@@ -20,6 +20,19 @@ type error_id =
     | ErrorIdUnexpectedAst
     | ErrorIdSyntaxError
 
+type error = {
+    mutable id: error_id CCVector.vector;
+    mutable line: int CCVector.vector;
+    mutable col: int CCVector.vector;
+    mutable count: int;
+}
+
+val new_error : error
+
+val push_error : error -> error_id -> int -> int -> unit
+
 val error_id_to_str : error_id -> string
 
 val print_error : error_id -> int -> int -> string -> unit
+
+val print_errors : error -> string -> int -> unit
