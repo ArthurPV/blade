546da08bd7aed53930f2046c7783f2d550d1156b
Arthur <arthurpontiervalois@gmail.com>
2021-05-25 22:59:41 -0400

    Recognize differentes platforms

--- src/command/command.ml
+++ src/command/command.ml
@@ -2,10 +2,14 @@ open Help
 open Run
 open Version
 
+external get_os : unit -> string = "get_os"
+external get_arch : unit -> string = "get_arch"
+
 module MainLilyCommand = struct
     let lily_help () = Printf.printf "%s" lily_h
 
     let lily_run filename () = run filename
 
-    let lily_version () = Printf.printf "blade v%s\n" lily_v
+    let lily_version () = 
+        Printf.printf "blade v%s on %s/%s\n" lily_v (get_os ()) (get_arch ())
 end
