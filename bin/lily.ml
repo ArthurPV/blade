0386524157a6fb86a31907770c3fee59d3e30de4
Arthur <arthurpontiervalois@gmail.com>
2021-05-29 21:28:37 -0400

    Remove tabs

--- bin/lily.ml
+++ bin/lily.ml
@@ -9,9 +9,10 @@ let _ =
            | "-h" | "--help" | "help" -> MainLilyCommand.lily_help ()
            | "init" -> Printf.printf "init"
            | "new" -> Printf.printf "new"
-           | "run" -> (match Array.length Sys.argv with
-                      | 2 -> Printf.printf "\027[1m\027[31mError\027\027[0m\027[1m: file doesn\'t specified\027\n"
-                      | _ -> MainLilyCommand.lily_run (Sys.argv.(2)) ())
+           | "run" -> (
+               match Array.length Sys.argv with
+               | 2 -> Printf.printf "\027[1m\027[31mError\027\027[0m\027[1m: file doesn\'t specified\027\n"
+               | _ -> MainLilyCommand.lily_run (Sys.argv.(2)) ())
            | "to" -> Printf.printf "to"
            | "-v" | "--version" | "version" -> MainLilyCommand.lily_version ()
            | _ -> Printf.printf "\027[1m\027[31mError\027\027[0m\027[1m: unexpected command: \'%s\'\027\n" Sys.argv.(1)
