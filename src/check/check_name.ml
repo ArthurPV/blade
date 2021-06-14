e64c5847295bd6aff3e000bd50e5325b3b57ea43
Arthur <arthurpontiervalois@gmail.com>
2021-06-13 23:12:03 -0400

    Update check stage

--- src/check/check_name.ml
+++ src/check/check_name.ml
@@ -1,6 +1,17 @@
 open LilyAst.Ast
 
 type check_name = {
+    ast: ast;
     mutable declaration: ast_kind CCVector.vector;
     mutable current_declaration: ast_kind;
+}
+
+let get_check_declaration ast = 
+    let v = CCVector.create () in
+    v
+
+let new_check_name ast decl = {
+    ast = ast;
+    declaration = decl;
+    current_declaration = CCVector.get decl 0;
 }
--- src/check/check_name.mli
+++ src/check/check_name.mli
@@ -1,6 +1,10 @@
 open LilyAst.Ast
 
 type check_name = {
+    ast: ast;
     mutable declaration: ast_kind CCVector.vector;
     mutable current_declaration: ast_kind;
 }
+
+val get_check_declaration : ast -> ast_kind CCVector.vector 
+val new_check_name : ast -> ast_kind CCVector.vector -> check_name
